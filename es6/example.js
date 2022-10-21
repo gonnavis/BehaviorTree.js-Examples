aed83959ad82362a06f4fad9911303b0f337b4d8
gonnavis <gonnavis@163.com>
2022-10-22 05:04:00 +0800

    test

--- es6/example.js
+++ es6/example.js
@@ -1,8 +1,15 @@
 import { BehaviorTree, Sequence, Task, SUCCESS, FAILURE, RUNNING } from 'behaviortree'
 
 BehaviorTree.register('bark', new Task({
+  start: function (dog) {
+    console.log('!!! 000 start')
+  },
+  end: function (dog) {
+    console.log('!!! 000 end')
+  },
   run: function (dog) {
     dog.bark()
+    console.log('000 SUCCESS')
     return SUCCESS
   }
 }))
@@ -13,31 +20,47 @@ const tree = new Sequence({
   nodes: [
     'bark',
     new Task({
+      start: function (dog) {
+        console.log('!!! 111 start')
+      },
+      end: function (dog) {
+        console.log('!!! 111 end')
+      },
       run: function (dog) {
         dog.randomlyWalk()
         testCount++;
         if (testCount <= 3) {
+          console.log('111 RUNNING')
           return RUNNING
         } else {
+          console.log('111 SUCCESS')
           return SUCCESS
         }
       }
     }),
-    'bark',
+    // 'bark',
     new Task({
+      start: function (dog) {
+        console.log('!!! 222 start')
+      },
+      end: function (dog) {
+        console.log('!!! 222 end')
+      },
       run: function (dog) {
-        if (dog.standBesideATree()) {
-          dog.liftALeg()
+        // if (dog.standBesideATree()) {
+          // dog.liftALeg()
           dog.pee()
           testCount2++;
           if (testCount2 <= 3) {
+            console.log('222 RUNNING')
             return RUNNING
           } else {
+            console.log('222 SUCCESS')
             return SUCCESS
           }
-        } else {
-          return FAILURE
-        }
+        // } else {
+        //   return FAILURE
+        // }
       }
     })
   ]
@@ -45,19 +68,22 @@ const tree = new Sequence({
 
 class Dog {
   bark () {
-    console.log('*wuff*')
+    // console.log('*wuff*')
+    // console.log('000')
   }
 
   randomlyWalk () {
-    console.log('The dog walks around.')
+    // console.log('The dog walks around.')
+    // console.log('111')
   }
 
   liftALeg () {
-    console.log('The dog lifts a leg.')
+    // console.log('The dog lifts a leg.')
   }
 
   pee () {
-    console.log('The dog pees.')
+    // console.log('The dog pees.')
+    // console.log('222')
   }
 
   standBesideATree () {
